ccab3e382b07a0f6b217e1fabe20f8ef3926b7cc
Jose Espejo <joseespejo@yahoo.com>
2024-02-27 16:50:39 +0000

    feat: add NewsDetail screen

---
+++ lib/src/screens/news_detail.dart
@@ -0,0 +1,86 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import '../blocs/comments_provider.dart';
+import '../models/item_model.dart';
+import '../widgets/comment.dart';
+
+class NewsDetail extends StatelessWidget {
+  final int itemId;
+
+  const NewsDetail({Key? key, required this.itemId}) : super(key: key);
+
+  @override
+  Widget build(context) {
+    final bloc = CommentsProvider.of(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Detail'),
+      ),
+      body: buildBody(bloc),
+    );
+  }
+
+  Widget buildBody(CommentsBloc bloc) {
+    return StreamBuilder(
+        stream: bloc.itemWithComments,
+        builder:
+            (context, AsyncSnapshot<Map<int, Future<ItemModel?>>> snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          final itemFuture = snapshot.data![itemId];
+
+          return FutureBuilder(
+              future: itemFuture,
+              builder: (context, AsyncSnapshot<ItemModel?> itemSnapshot) {
+                if (!itemSnapshot.hasData) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else {
+                  return buildList(itemSnapshot.data!, snapshot.data!);
+                  // return buildTitle(itemSnapshot.data!);
+                }
+              });
+        });
+  }
+
+  Widget buildTitle(ItemModel item) {
+    return Container(
+      margin: const EdgeInsets.all(10.0),
+      alignment: Alignment.topCenter,
+      child: Text(
+        item.title,
+        textAlign: TextAlign.center,
+        style: const TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+
+  Widget buildList(ItemModel item, Map<int, Future<ItemModel?>> itemMap) {
+    final children = <Widget>[];
+    children.add(buildTitle(item));
+
+    final commentsList = item.kids.map((kidId) {
+      return Comment(itemId: kidId, itemMap: itemMap, depth: 0);
+    }).toList();
+
+    children.addAll(commentsList);
+
+    return ListView(children: children);
+
+    /* 
+
+    for (var kidId in item.kids) {
+      children.add(NewsListTile(itemId: kidId));
+    }
+
+    return ListView(
+      children: children,
+    ); */
+  }
+}
